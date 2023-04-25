c53ba7c54e61f1c708f0637ab8e4593e307ac6f2
Hassan Chamseddine <hassanchamseddine92@outlook.com>
2023-04-25 11:18:58 +0200

    other changes

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,60 @@
+import Layout from '@/components/layout'
+import { Store } from '@/utils/Store';
+import data from '@/utils/data';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+import React, { useContext } from 'react'
+
+export default function ProductDetails() {
+    const { state, dispatch } = useContext<any>(Store);
+    const { query } = useRouter();
+    const { slug } = query;
+    const product = data.products.find(x => x.slug === slug);
+    if (!product) {
+        return <div>Product Not Fo und</div>
+    }
+    const addToCartHandler = () => {
+        const existItem = state.cart.cartItems.find((x: any) => x.slug === slug)
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+
+        if (product.InStock < quantity) {
+            alert('Sorry, product is out of stock!')
+        }
+        dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: quantity } });
+    }
+    return (
+        <Layout title={product.name}>
+            <div className='py-2'>
+                <Link href='/'>back to products</Link>
+            </div>
+            <div className='grid md:grid-cols-4 md:gap-3'>
+                <div className='md:col-span-2'>
+                    <Image src={product.image} alt={product.name} width={640} height={640}></Image>
+                </div>
+                <div>
+                    <ul>
+                        <li><h1 className='text-lg'>{product.name}</h1></li>
+                        <li>Category: {product.category}</li>
+                        <li>Brand: {product.brand}</li>
+                        <li>{product.rating} of {product.reviews} reviews</li>
+                        <li>Description: {product.description}</li>
+                    </ul>
+                </div>
+                <div>
+                    <div className='card p-5'>
+                        <div className='mb-2 flex justify-between'>
+                            <div>Price</div>
+                            <div>${product.price}</div>
+                        </div>
+                        <div className='mb-2 flex justify-between'>
+                            <div>Status</div>
+                            <div>{product.InStock > 0 ? 'In Stock' : 'Unavailable'}</div>
+                        </div>
+                        <button className='primary-button w-full' onClick={addToCartHandler}>Add to cart</button>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
---
+++ utils/data.tsx
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        // {
+        //     name: 'box 1',
+        //     slug: 'box-1',
+        //     category: 'boxes',
+        //     image:'/images/box1.png',
+        //     price: 139,
+        //     brand:'Ikea',
+        //     rating: 3.5,
+        //     reviews: 8,
+        //     InStock:10,
+        //     description: 'Fit your place'
+        // },
+        // {
+        //     name: 'box 2',
+        //     slug: 'box-2',
+        //     category: 'boxes',
+        //     image:'/images/box2.png',
+        //     price: 149,
+        //     brand:'Ikea',
+        //     rating: 4.5,
+        //     reviews: 3,
+        //     InStock:20,
+        //     description: 'Fit your place'
+        // },
+        {
+            name: 'box 3',
+            slug: 'box-3',
+            category: 'boxes',
+            image:'/images/box3.png',
+            price: 159,
+            brand:'Ikea',
+            rating: 2.5,
+            reviews: 1,
+            InStock:19,
+            description: 'Fit your place'
+        },
+        {
+            name: 'box 4',
+            slug: 'box-4',
+            category: 'boxes',
+            image:'/images/box4.png',
+            price: 169,
+            brand:'Ikea',
+            rating: 3.5,
+            reviews: 5,
+            InStock:12,
+            description: 'Fit your place'
+        },
+        {
+            name: 'box 5',
+            slug: 'box-5',
+            category: 'boxes',
+            image:'/images/box5.png',
+            price: 179,
+            brand:'Ikea',
+            rating: 3.5,
+            reviews: 8,
+            InStock:10,
+            description: 'Fit your place'
+        },
+        // {
+        //     name: 'box 6',
+        //     slug: 'box-6',
+        //     category: 'boxes',
+        //     image:'/images/box6.png',
+        //     price: 139,
+        //     brand:'Ikea',
+        //     rating: 3.5,
+        //     reviews: 8,
+        //     InStock:10,
+        //     description: 'Fit your place'
+        // },
+    ]
+};
+
+export default data;
