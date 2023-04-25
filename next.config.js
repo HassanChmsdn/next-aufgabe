87f5c6d7a625e23bb62261accfce400d62c34ef3
Hassan Chamseddine <hassanchamseddine92@outlook.com>
2023-04-25 12:04:31 +0200

    fix error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'out',
+  // output: 'export',
+  // distDir: 'out',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
